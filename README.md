389de312fdf29ed07700522350c9259c5d36983d
Chris <cmvee@users.noreply.github.com>
2018-01-08 01:10:16 -0500

    shift to typescript; addressed outstanding issues

--- README.md
+++ README.md
@@ -11,45 +11,87 @@ A node.js module that uses the Linux-based BlueZ package's `l2ping` binary to de
 ## Installation
 `npm install --save bt-presence`
 
+### A Note on Requirement for Root Priveleges
+
+The `l2ping` linux binary requires root priveleges in order to write to bluetooth. You have 2 options for addressing this issue:
+1. Run ``sudo setcap 'cap_net_raw+eip' `which l2ping` `` in order to grant root priveleges to `l2ping`
+2. Run whichever software you are integrating `bt-presence` into as a user with root priveleges
+
+Option #1 is recommended since it prevents bt-presence (and your code that may depend on it) from needing to run with escalated priveleges.
+
 ## Usage
 
 ### Including the package
-`const btPresence = require('bt-presence')`
+`const btPresence = require('bt-presence').btPresence`
+
+### Instantiating a New Scanner
+You must instantiate a new instance of btPresence prior to use
+
+`let btp = new btPresence()`
 
 ### Adding Addresses to Scan
 You can add addresses to scan by supplying an array of MAC addresses. The devices in the supplied array are concatenated to, and do not replace, any existing devices that may have already been added.
 
-`btPresence.addDevices(["00:12:34:56:78:90", "09:87:65:43:21:00"])`
+`btp.addDevices(["00:12:34:56:78:90", "09:87:65:43:21:00"])`
 
 ### Removing Addresses from Scan
 You can remove addresses from the scan by supplying an array of MAC addresses.
 
-`btPresence.removeDevices(["00:12:34:56:78:90", "09:87:65:43:21:00"])`
+`btp.removeDevices(["00:12:34:56:78:90", "09:87:65:43:21:00"])`
 
 ### Setting/Replacing List of Addresses to Scan
 You can replace the entire list of addresses to scan by supplying an array of MAC addresses.
 
-`btPresence.setDevices(["00:12:34:56:78:90", "09:87:65:43:21:00"])`
+`btp.setDevices(["00:12:34:56:78:90", "09:87:65:43:21:00"])`
+
+### Getting List of Addresses to Scan
+Returns the entire list of addresses to scan.
+
+`btp.getDevices()`
+
+### Configuring Ping Options
+If you desire to change the parameters used with `l2ping` (for ping count and timeout):
+
+`btp.setScanOptions({ count: 3, timeoutSecs: 8 })`
+
+### Getting Ping Options
+Returns the current `l2ping` scan options
+
+`btp.getPingOptions()`
 
 ### Detecting Change: from No Devices Present to 1+ Devices Present
 Subscribe to the `'present'` event in order to know when 1 or more devices have appeared after a state where none were present prior.
 
-``btPresence.on('present', (macAddress) => console.log(`A device [${macAddress}] is now present`))``
+``btp.on('present', (macAddress) => console.log(`A device [${macAddress}] is now present`))``
 
 ### Detecting Change: from No Devices Present to 1+ Devices Present
 Subscribe to the `'not-present'` event in order to know when all devices are gone.
 
-``btPresence.on('not-present', (macAddress) => console.log(`All devices have disappeared`))``
+``btp.on('not-present', (macAddress) => console.log(`All devices have disappeared`))``
 
 ### Starting the Scan
+This starts the scan. By default, the first positive or negative reponse from any device in a newly started scan will be emitted as a change. To disable this, call `btp.start(false)`.
 
-`btPresence.start()`
+`btp.start(true)`
 
 ### Stopping the Scan
 
-`btPresence.stop()`
+`btp.stop()`
 
 ### Adjusting the Scan Interval
 You can adjust the time interval upon which the entire list of devices is pinged to detect presence or absence. The default scan interval is 15 seconds; use the function below to adjust it. The underlying ping tool, `l2ping`, takes around 2-3 secs to complete when a device is present, and 5-10 secs to complete when a device is not present. So, consider how many devices you are scanning before you set this.
 
-`btPresence.setScanInterval(60)`
+`btp.setScanInterval(60)`
+
+### Getting the Scan Interval
+Returns the number of seconds between scans
+
+`btp.getScanInterval()`
+
+## Contributing / Modifying
+The project is now developed in TypeScript. To modify the code for your own use:
+
+1. Clone the repo with `git clone https://github.com/cmvee/bt-presence.git`
+2. Edit the `.ts` files in the `src` directory
+3. `npm run build` to transpile the TypeScript `.ts` source files into Javascript
+4. Look in the `dist` directory for the fresh Javascript files
---
+++ src/index.ts
@@ -0,0 +1,269 @@
+'use strict';
+
+import { setTimeout } from "timers";
+import { EventEmitter } from "events";
+import * as _ from "lodash";
+import { spawn, ChildProcess } from 'child_process';
+
+interface L2pingOptions {
+    count : number;
+    timeoutSecs : number;
+}
+
+interface PingResult {
+    address : string;
+    isPresent : boolean;
+}
+
+class btPresence extends EventEmitter {
+
+    // stores bluetooth MAC addresses of devices to ping
+    private btDevicesToPing: Array<string> = [];
+
+    // stores bluetooth MAC addresses of detected devices
+    private btDevicesPresent: Array<string> = [];
+
+    // default time between scanning for list of devices
+    private scanIntervalSecs: number = 15;
+
+    // serves as a cached status reflecting whether l2ping is available on this system
+    private isL2PingAvailable: boolean = false;
+
+    // the timer used by setTimeout or setInterval; track it so we can clear it if needed
+    private timer: NodeJS.Timer;
+
+    // whether or not to emit an event on the first scan regardless of change
+    private reportFirstResult: boolean = true;
+
+    // tracks the running ping processes
+    private runningPings: Array<ChildProcess> = [];
+
+    // parameters for the l2ping call
+    private l2pingOptions: L2pingOptions = {
+        count: 1,
+        timeoutSecs: 5
+    };
+
+    constructor (){
+        super();
+    }
+    
+    /**
+     * Checks for the presence of l2ping on the local system
+     * @param {function} next the calback function to call if l2ping is present
+     */
+    private checkForL2Ping = (next: Function) => {
+        var process = spawn('which', ['l2ping']);
+        
+        process.on("error", (error) => {
+            console.error(`Error trying to locate l2ping: `);
+            throw error;
+        });
+
+        process.on("exit", (code, signal) => {
+            // update the variable other functions check
+            this.isL2PingAvailable = code === 0 ? true : false;
+
+            if (this.isL2PingAvailable){
+                if (next) next();
+            } else {
+                throw new Error(`dependency l2ping is not installed on the local system or is not on the PATH`);
+            }
+        });
+    }
+
+    /**
+     * Adds MAC addresses to the list of devices whose presence we want to detect
+     * @param {array} btMacAddressArray an array of bluetooth MAC addresses to add
+     */
+    public addDevices = (btMacAddressArray: Array<string>) => {
+        if (typeof btMacAddressArray != 'undefined'){
+            const macsLowerCase = _.map(btMacAddressArray, _.toLower);
+            this.btDevicesToPing = _.uniq( _.concat(this.btDevicesToPing, macsLowerCase) );
+        }
+    }
+
+    /**
+     * Removes MAC addresses from the list of devices whose presence we want to detect
+     * @param {array} btMacAddressArray an array of bluetooth MAC addresses to remove
+     */
+    public removeDevices = (btMacAddressArray: Array<string>) => {
+        if (typeof btMacAddressArray != 'undefined'){
+            const macsLowerCase = _.map(btMacAddressArray, _.toLower);
+            this.btDevicesToPing = _.uniq( _.pullAll(this.btDevicesToPing, macsLowerCase) );
+        }
+    }
+
+    /**
+     * Returns list of MAC addresses of devices whose presence we want to detect
+     */
+    public getDevices = () => {
+        return this.btDevicesToPing;
+    }
+
+    /**
+     * Replaces list of MAC addresses of devices whose presence we want to detect
+     * @param {array} btMacAddressArray an array of bluetooth MAC addresses to detect
+     */
+    public setDevices = (btMacAddressArray: Array<string>) => {
+        const macsLowerCase = _.map(btMacAddressArray, _.toLower);
+        this.btDevicesToPing = _.uniq(macsLowerCase);
+    }
+
+    /**
+     * Returns an object with the options for l2ping
+     */
+    public getPingOptions = () => {
+        return this.l2pingOptions;
+    }
+
+    /**
+     * Sets the l2ping options
+     * @param {Object} options an object with "count" and "timeoutSecs" values
+     */
+    public setPingOptions = (options: L2pingOptions) => {
+        var newOptions: L2pingOptions = {
+            count: options.count ? options.count : 1,
+            timeoutSecs: options.timeoutSecs ? options.timeoutSecs : 5
+        };
+
+        this.l2pingOptions = newOptions;
+    }
+
+    /**
+     * Returns the number of seconds between successive scans
+     */
+    public getIntervalSeconds = () => {
+        return this.scanIntervalSecs;
+    }
+
+    /**
+     * Sets the number of seconds between successive scans
+     * @param {number} secs the number of seconds between successive scans
+     */
+    public setIntervalSeconds = (secs: number) => {
+        if (typeof secs != 'undefined')
+            this.scanIntervalSecs = secs;
+    }
+
+    /**
+     * Starts a repeated scan through the list of bluetooth devices.
+     * NOTE: this function is NOT intended to be called by external code.
+     */
+    private scanForAllDevices = () => {
+        if (this.btDevicesToPing.length <= 0) {
+            // console.log(`[${new Date().toISOString()}] no BT devices to ping`);
+        } else {
+            // console.log(`[${new Date().toISOString()}] scanning for presence of any of ${this.btDevicesToPing.length} BT devices`);
+
+            // scan for each device
+            _.forEach(this.btDevicesToPing, (macAddress) => {
+                this.pingBluetoothDevice(macAddress, this.onPingResult.bind(this));
+            });
+        }
+
+        // sleep for interval and then repeat the scan
+        this.timer = setTimeout(this.scanForAllDevices.bind(this), this.scanIntervalSecs * 1000);
+    }
+
+    /**
+     * This verifies that l2ping is available and then starts the continuous scan.
+     * NOTE: This function should be called by external code in order to start scans.
+     * * @param {boolean} reportFirstResult whether to report the first scan result regardless of change in presence
+     */
+    public start = (reportFirstResult?: boolean) => {
+
+        if (typeof reportFirstResult != 'undefined'){
+            this.reportFirstResult = reportFirstResult;
+        }
+
+        if (this.isL2PingAvailable){
+            this.scanForAllDevices.apply(this);
+        } else {
+            this.checkForL2Ping(this.scanForAllDevices.bind(this));
+        }
+    }
+
+    /**
+     * Stops any continuous scan that may be running
+     */
+    public stop = () => {
+        if (typeof this.timer != 'undefined'){
+            // stop any future execution
+            clearTimeout(this.timer);
+
+            // kill any remaining processes
+            _.forEach(this.runningPings, (p: ChildProcess) => {
+                if (!p.killed) {
+                    p.kill();
+                }
+            });
+        }
+    }
+
+    /**
+     * Uses the l2ping binary on the local system to determine if a bluetooth device is within range
+     * @param {string} macAddress the MAC of the bluetooth device to ping
+     * @param {function} callback the callback function
+     */
+    private pingBluetoothDevice = (macAddress: string, callback: Function) => {
+        // start a process to ping the device
+        var process = spawn('l2ping', ['-c', String(this.l2pingOptions.count), '-t', String(this.l2pingOptions.timeoutSecs), macAddress]);
+        
+        // add to the list of running processes
+        this.runningPings.push(process);
+
+        process.on("error", (error) => {
+            console.error("Error running l2ping: ");
+            throw error;
+        });
+    
+        // l2ping writes to stdout if the device is found
+        process.stdout.on('data', (data) => {
+            if (callback) callback({ address: macAddress, isPresent: true });
+        });
+    
+        // l2ping writes to stderr if the device is not found
+        process.stderr.on('data', () => {
+            if (callback) callback({ address: macAddress, isPresent: false });
+        });
+
+        process.on("close", (code, signal) => {
+            // remove from the list of running processes
+            _.remove(this.runningPings, (p: ChildProcess) => {
+                return p.pid == process.pid;
+            });
+        })
+    }
+
+    /**
+     * Callback function executed when l2ping has returned with a result
+     * @param {object} result an object containing the result of the ping
+     */
+    public onPingResult = (result: PingResult) => {
+        // save whether any devices were present prior to this ping result
+        const devicesPresentAtLastCheck = this.btDevicesPresent.length > 0;
+
+        // update the device presence list based on the result of the ping
+        if (result.isPresent){
+            this.btDevicesPresent = _.uniq(_.concat(this.btDevicesPresent, result.address));
+        } else {
+            this.btDevicesPresent = _.pull(this.btDevicesPresent, result.address);
+        }
+
+        // detect changes in presence and emit events if necessary
+        if (this.btDevicesPresent.length > 0){
+            if (!devicesPresentAtLastCheck || this.reportFirstResult){
+                this.emit('present', result.address);
+            }
+        } else {
+            if (devicesPresentAtLastCheck || this.reportFirstResult){
+                this.emit('not-present', result.address);
+            }
+        }
+
+        this.reportFirstResult = false;
+    }
+}
+
+export { L2pingOptions, PingResult, btPresence };
